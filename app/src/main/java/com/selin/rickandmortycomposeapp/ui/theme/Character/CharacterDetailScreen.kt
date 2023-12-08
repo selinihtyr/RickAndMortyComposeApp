a1eabe72008872541dafbb713d388b54bf6c5185
selinihtyr <nurselinihtiyar@gmail.com>
2023-12-08 11:48:57 +0300

    character detail screen created

---
+++ app/src/main/java/com/selin/rickandmortycomposeapp/ui/theme/Character/CharacterDetailScreen.kt
@@ -0,0 +1,17 @@
+package com.selin.rickandmortycomposeapp.ui.theme.Character
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.skydoves.landscapist.glide.GlideImage
+
+
+@Composable
+fun DetailScreen(viewModel: CharacterViewModel = hiltViewModel()) {
+
+}
