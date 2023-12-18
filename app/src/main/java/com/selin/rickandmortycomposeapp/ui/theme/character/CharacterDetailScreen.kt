242ec58f8e6b3464630d26d62b5668cae069196e
selinihtyr <nurselinihtiyar@gmail.com>
2023-12-18 21:27:12 +0300

    CharacterDetailScreen.kt design completed.

--- app/src/main/java/com/selin/rickandmortycomposeapp/ui/theme/character/CharacterDetailScreen.kt
+++ app/src/main/java/com/selin/rickandmortycomposeapp/ui/theme/character/CharacterDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.selin.rickandmortycomposeapp.ui.theme.character
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,15 +9,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -48,154 +42,143 @@ fun DetailScreen(
     }
 
     LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
+        modifier = Modifier.fillMaxSize()
     ) {
         item {
             Box(modifier = Modifier.fillMaxSize()) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(start = 16.dp, end = 16.dp)
-                ) {
-                    GlideImage(
-                        imageModel = character.value?.image ?: "",
-                        contentDescription = "Localized description",
-                    )
-                }
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 32.dp, end = 32.dp, top = 330.dp)
-                ) {
-                    Column(
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = character.value?.name ?: "",
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Divider(
-                            color = Color.DarkGray,
-                            thickness = 1.dp,
-                            modifier = Modifier.padding(top = 8.dp, start = 24.dp, end = 24.dp)
+                CharacterImage(character = character.value)
+                CharacterMainInfo(character = character.value)
+            }
+            CharacterInfoCard("Location", character.value?.location?.name ?: "")
+            CharacterInfoCard("Origin", character.value?.origin?.name ?: "")
+            EpisodeCard(character = character.value, navController = navController)
+        }
+    }
+}
 
-                        )
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceEvenly,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Column {
-                                Icon(
-                                    imageVector = Icons.Default.Person,
-                                    contentDescription = "Person Icon",
-                                    tint = Color.Black,
-                                    modifier = Modifier.size(48.dp)
-                                )
-                                Text(
-                                    text = character.value?.species ?: "",
-                                )
-                            }
-                            Column {
-                                Icon(
-                                    imageVector = Icons.Default.Info,
-                                    contentDescription = "Person Icon",
-                                    tint = Color.Black,
-                                    modifier = Modifier.size(48.dp)
-                                )
-                                Text(
-                                    text = character.value?.status ?: "",
-                                )
-                            }
-                            Column {
-                                Icon(
-                                    imageVector = Icons.Default.Face,
-                                    contentDescription = "Person Icon",
-                                    tint = Color.Black,
-                                    modifier = Modifier.size(48.dp)
-                                )
-                                Text(
-                                    text = character.value?.gender ?: "",
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-            Card(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = 24.dp, end = 24.dp, top = 4.dp)
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(
-                        text = "Location",
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Unspecified
-                    )
-                    Text(
-                        text = character.value?.location?.name ?: "",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-            }
-            Card(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = 24.dp, end = 24.dp, top = 4.dp)
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Origin", fontSize = 14.sp, fontWeight = FontWeight.Bold)
-                    Text(
-                        text = character.value?.origin?.name ?: "",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-            }
-            Card(modifier = Modifier
-                .fillMaxSize()
-                .padding(start = 24.dp, end = 24.dp, top = 4.dp)
-                .clickable {
-                    val episodeIds = character.value?.episode?.map {
-                        it
-                            .substringAfterLast("/")
-                            .toInt()
-                    } ?: emptyList()
-                    navController.navigate("episodesFromCharacter/${episodeIds.joinToString(",")}")
-                    Log.d("EpisodeIds", episodeIds.joinToString(","))
-                }) {
-                Column(
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Episodes", fontSize = 14.sp, fontWeight = FontWeight.Bold)
+@Composable
+fun CharacterImage(character: CharacterResponseList?) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp)
+    ) {
+        GlideImage(
+            imageModel = character?.image ?: "",
+            contentDescription = "Localized description",
+        )
+    }
+}
 
-                }
+@Composable
+fun CharacterMainInfo(character: CharacterResponseList?) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 32.dp, end = 32.dp, top = 330.dp)
+            .background(Color.Transparent),
+        elevation = CardDefaults.cardElevation(5.dp),
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.background(Color.White)
+        ) {
+            Text(
+                text = character?.name ?: "",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Divider(
+                color = Color.DarkGray,
+                thickness = 1.dp,
+                modifier = Modifier.padding(top = 8.dp, start = 24.dp, end = 24.dp)
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                CharacterAttribute(character?.species ?: "")
+                CharacterAttribute(character?.status ?: "")
+                CharacterAttribute(character?.gender ?: "")
             }
         }
     }
 }
 
+@Composable
+fun CharacterAttribute(text: String) {
+    Column {
+        Text(
+            text = text,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+        )
+    }
+}
 
+@Composable
+fun CharacterInfoCard(title: String, content: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, end = 24.dp, top = 4.dp)
+            .background(Color.Transparent),
+        elevation = CardDefaults.cardElevation(5.dp),
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+        ) {
+            Text(
+                text = title,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Unspecified,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = content,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun EpisodeCard(character: CharacterResponseList?, navController: NavController) {
+    Card(
+        elevation = CardDefaults.cardElevation(5.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, end = 24.dp, top = 4.dp)
+            .clickable {
+                val episodeIds = character?.episode?.map {
+                    it
+                        .substringAfterLast("/")
+                        .toInt()
+                } ?: emptyList()
+                navController.navigate("episodesFromCharacter/${episodeIds.joinToString(",")}")
+            }
+    ) {
+        Text(
+            text = "Episodes",
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.White)
+                .align(Alignment.CenterHorizontally),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+        )
+    }
+}
